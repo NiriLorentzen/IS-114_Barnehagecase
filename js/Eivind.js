76e2b29dca6388cf669149a4ddbc1741f0385948
Niri <niri.lorentzen@gmail.com>
2023-11-21 18:27:05 +0100

    asodansodan

--- js/Eivind.js
+++ js/Eivind.js
@@ -1,9 +1,9 @@
 // Importerer funksjoner fra figures.js
-import {circleCanvas} from "https://nirilorentzen.github.io/IS-114_Barnehagecase/js/figures.js";
-import {rectangleCanvas} from "https://nirilorentzen.github.io/IS-114_Barnehagecase/js/figures.js";
+import {circleCanvas} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
+import {rectangleCanvas} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
 
-import {rectangleSVG} from "https://nirilorentzen.github.io/IS-114_Barnehagecase/js/figures.js";
-import {circleSVG} from "https://nirilorentzen.github.io/IS-114_Barnehagecase/js/figures.js";
+import {rectangleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
+import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
 
 // Sverd med Canvas API
 // Lage tuppen av sverdet
