46d5d9f803ba7e14c253ba4e5728b8b559d8dc6e
Niri <niri.lorentzen@gmail.com>
2023-11-21 18:50:00 +0100

    sadiadisjn

--- js/Kristian.js
+++ js/Kristian.js
@@ -8,9 +8,9 @@ import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js"
 function draw_canvas(){
 rectangleCanvas(400, 400, "solid", "white", 0, 0);
 circleCanvas(50, "solid", "yellow", 125, 65);
-rectangleCanvas(20, 5, "solid", "black", 100, 75);
-circleCanvas(15, "solid", "black", 45, 45);
-circleCanvas(15, "solid", "black", 90, 45);
+rectangleCanvas(20, 5, "solid", "black", 120, 75);
+circleCanvas(15, "solid", "black", 115, 50);
+circleCanvas(15, "solid", "black", 140, 50);
 }
 
 draw_canvas()
