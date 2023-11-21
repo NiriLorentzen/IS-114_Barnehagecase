7e77ce93429d7ae1fc1f351628927b551994add8
Niri <niri.lorentzen@gmail.com>
2023-11-21 16:33:04 +0100

    kristian endra noe

--- js/Kristian.js
+++ js/Kristian.js
@@ -3,3 +3,19 @@ import {rectangleCanvas} from "https://nirilorentzen.github.io/gruppe9b/js/figur
 
 import {rectangleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
 import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
+
+
+function draw_canvas(){
+rectangleCanvas(400, 400, "solid", "black", 0, 0);
+circleCanvas(150, 150, "solid", "red", 125, 65);
+}
+
+draw_canvas()
+
+
+function draw_svg(){
+rectangleSVG(400, 400, "solid", "black", 0, 0);
+circleSVG(150, 150, "solid", "red", 125, 65);
+}
+
+draw_svg()
