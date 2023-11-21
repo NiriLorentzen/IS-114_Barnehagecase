e677d4c319c8c710051f9ae5623d6ca2f434008a
Niri <niri.lorentzen@gmail.com>
2023-11-21 17:44:06 +0100

    asdoasdoasdn

--- js/Kristian.js
+++ js/Kristian.js
@@ -7,7 +7,8 @@ import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js"
 
 function draw_canvas(){
 rectangleCanvas(400, 400, "solid", "white", 0, 0);
-circleCanvas(30, "solid", "yellow", 125, 65);
+circleCanvas(50, "solid", "yellow", 125, 65);
+rectangleCanvas(20, 5, "solid", "black", 100, 75);
 }
 
 draw_canvas()
@@ -16,6 +17,7 @@ draw_canvas()
 function draw_svg(){
 rectangleSVG(400, 400, "solid", "white", 0, 0);
 circleSVG(50, "solid", "yellow", 125, 65);
+rectangleSVG(20, 5, "solid", "black", 100, 75);
 }
 
 draw_svg()
