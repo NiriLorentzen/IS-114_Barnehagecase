85db76509a038de87fa5ce6894cba3072244a451
Niri <niri.lorentzen@gmail.com>
2023-11-21 17:52:34 +0100

    oasdaosudn

--- js/Kristoffer.js
+++ js/Kristoffer.js
@@ -6,7 +6,6 @@ import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js"
 
 function draw_canvas(){
     //circleCanvas(70, "solid, "blue", 100, 100);
-
     rectangleCanvas(999, 900, "solid", "black", 0, 0);
     circleCanvas(50, "solid", "grey", 140, 70);
     circleCanvas(20, "solid", "black", 120, 80); 
