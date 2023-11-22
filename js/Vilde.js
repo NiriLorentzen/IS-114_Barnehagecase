f4dad2acbb134634571020eb8c5c241e88721efb
Niri <niri.lorentzen@gmail.com>
2023-11-22 17:00:51 +0100

    litt diverse

--- js/Vilde.js
+++ js/Vilde.js
@@ -5,7 +5,7 @@ import {rectangleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.
 import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
 
 function draw_canvas(){
-    rectangleCanvas(200, 200, "solid", "grey", 0, 0);
+    rectangleCanvas(200, 200, "solid", "#d3d3d3", 0, 0);
     rectangleCanvas(10, 100, "solid", "white", 95, 60);
     circleCanvas(35, "solid", "pink", 100, 40);
     }
@@ -13,7 +13,7 @@ function draw_canvas(){
 draw_canvas()
 
  function draw_svg(){
-    rectangleSVG(200, 200, "solid", "grey", 0, 0);
+    rectangleSVG(200, 200, "solid", "#d3d3d3", 0, 0);
      rectangleSVG(10, 100, "solid", "white", 95, 60);
      circleSVG(35, "solid", "pink", 100, 40);
 
