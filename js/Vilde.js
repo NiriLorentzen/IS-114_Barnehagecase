ac69a046a1eee3c8777977065f3fd4e588feb60b
NiriLorentzen <144032550+NiriLorentzen@users.noreply.github.com>
2023-11-22 16:38:02 +0100

    Update Vilde.js
    endret tull

--- js/Vilde.js
+++ js/Vilde.js
@@ -6,16 +6,16 @@ import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js"
 
 function draw_canvas(){
     rectangleCanvas(200, 200, "solid", "grey", 0, 0);
-    circleCanvas(35, "solid", "pink", 100, 40);
     rectangleCanvas(10, 100, "solid", "white", 95, 60);
+    circleCanvas(35, "solid", "pink", 100, 40);
     }
  
 draw_canvas()
 
  function draw_svg(){
     rectangleSVG(200, 200, "solid", "grey", 0, 0);
-     circleSVG(35, "solid", "pink", 100, 40);
      rectangleSVG(10, 100, "solid", "white", 95, 60);
+     circleSVG(35, "solid", "pink", 100, 40);
 
      }
         
