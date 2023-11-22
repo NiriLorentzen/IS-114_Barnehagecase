0ba62ba12b85f64500c87257ff49cf065e1ce486
Vilde Christensen <144059333+Vildech@users.noreply.github.com>
2023-11-22 16:22:40 +0100

    Update Vilde.js

--- js/Vilde.js
+++ js/Vilde.js
@@ -6,16 +6,16 @@ import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js"
 
 function draw_canvas(){
     rectangleCanvas(200, 200, "solid", "grey", 0, 0);
-    circleCanvas(35, "solid", "pink", 100, 10);
-    rectangleCanvas(10, 100, "solid", "white", 100, 45);
+    circleCanvas(35, "solid", "pink", 100, 20);
+    rectangleCanvas(10, 100, "solid", "white", 95, 35);
     }
  
 draw_canvas()
 
  function draw_svg(){
     rectangleSVG(200, 200, "solid", "grey", 0, 0);
-     circleSVG(35, "solid", "pink", 100, 10);
-     rectangleSVG(10, 100, "solid", "white", 100, 45);
+     circleSVG(35, "solid", "pink", 100, 20);
+     rectangleSVG(10, 100, "solid", "white", 99, 35);
 
      }
         
