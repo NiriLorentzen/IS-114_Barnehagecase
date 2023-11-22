035f5b0a72f009fad56f2b2770ef2b9ffd2a75c7
Ahmet Kirac <142392555+amtkrc@users.noreply.github.com>
2023-11-22 13:48:22 +0100

    v2

--- js/Achmed.js
+++ js/Achmed.js
@@ -3,3 +3,42 @@ import {rectangleCanvas} from "https://nirilorentzen.github.io/gruppe9b/js/figur
 
 import {rectangleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
 import {circleSVG} from "https://nirilorentzen.github.io/gruppe9b/js/figures.js";
+
+
+
+var canvas = document.getElementById('iceCreamCanvas');
+var context = canvas.getContext('2d');
+
+
+function drawIceCream() {
+    context.fillStyle = 'gold';
+
+    context.beginPath();
+    context.moveTo(200, 100);
+    context.lineTo(150, 250);
+    context.lineTo(250, 250);
+    context.fill();
+
+    context.beginPath();
+    context.arc(200, 100, 50, 0, Math.PI * 2);
+    context.fill();
+}
+
+function drawCone() {
+    context.fillStyle = '#8e44ad';
+
+    context.beginPath();
+    context.moveTo(150, 250);
+    context.lineTo(200, 400);
+    context.lineTo(250, 250);
+    context.fill();
+}
+
+function drawIceCreamCone() {
+    drawCone();
+    drawIceCream();
+}
+
+window.onload = function() {
+    drawIceCreamCone();
+};
